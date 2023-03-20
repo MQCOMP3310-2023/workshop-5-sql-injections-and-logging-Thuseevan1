45553798e3e779e6765275f93b224cb1a6c34be7
Thuseevan1 <88169000+Thuseevan1@users.noreply.github.com>
2023-03-20 17:41:34 +1100

    test

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -127,13 +127,19 @@ public class SQLiteConnectionManager {
      */
     public void addValidWord(int id, String word) {
 
-        String sql = "INSERT INTO validWords(id,word) VALUES('" + id + "','" + word + "')";
+        String sql = "INSERT INTO validWords(id,word) VALUES(?,?)";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                    pstmt.setInt(1, id);
+
+                    pstmt.setString(2, word);
+
             pstmt.executeUpdate();
+
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            logger.log(Level.SEVERE, "Error adding valid word to database", e);
+            // System.out.println(e.getMessage());
         }
 
     }
@@ -145,10 +151,13 @@ public class SQLiteConnectionManager {
      * @return true if guess exists in the database, false otherwise
      */
     public boolean isValidWord(String guess) {
-        String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
+        String sql = "SELECT count(id) as total FROM validWords WHERE word like ? ";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+                    stmt.setString(1, guess);
+
 
             ResultSet resultRows = stmt.executeQuery();
             if (resultRows.next()) {
@@ -159,7 +168,8 @@ public class SQLiteConnectionManager {
             return false;
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            logger.log(Level.SEVERE, "An exception occurred", e);
+            // System.out.println(e.getMessage());
             return false;
         }
 
